4815a25b181ff726f6c1842ba8b64522097c52d9
User <User@DESKTOP-QB4KLAE>
2023-03-19 16:45:08 +0900

    초기 커밋.

---
+++ WAGI_week2/WAGI_week2.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    
+    cout << "----------------------------------" << endl;
+    cout << "l                                l" << endl;
+    cout << "l    Name :  Jeon So Hyun        l" << endl;
+    cout << "l    Student number : 20221173   l" << endl;
+    cout << "l    Major: computer engineering l" << endl;
+    cout << "l    Age :  22                   l" << endl;
+    cout << "l    Birthday : 01.02            l" << endl;
+    cout << "l                                l" << endl;
+    cout << "----------------------------------" << endl;
+    
+    /*
+    cout << "Welcome to C++!" << endl;
+    cout << "Welcome to C++!" << endl;
+    cout << "Welcome to C++!" << endl;
+    cout << "Welcome to C++!" << endl;
+    cout << "Welcome to C++!" << endl;
+
+    cout << "\"@#$%^&*()\"" << endl;*/
+
+    return 0;
+
+}
